5e94c8abd2e2773416665645c0ce83cf2e9fe033
Richard Ebo <richardebboh@gmail.com>
2024-10-14 09:20:50 +0100

    Project setup

--- app/src/main/java/com/solid/udemyfreecourses/MainActivity.kt
+++ app/src/main/java/com/solid/udemyfreecourses/MainActivity.kt
@@ -4,14 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.solid.udemyfreecourses.ui.theme.UdemyFreeCoursesTheme
+import com.solid.udemyfreecourses.ui.utils.UdemyFreeCoursesPreview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,8 +31,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             UdemyFreeCoursesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    SplashScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +41,44 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun SplashScreen(
+    modifier: Modifier = Modifier
+) {
+    Column(
         modifier = modifier
-    )
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Text(
+                text = "Cancel",
+                modifier = modifier.clickable { println("Cancel was clicked") }
+            )
+
+            Text(
+                text = "Reset",
+                modifier = modifier.clickable { println("Reset was clicked") }
+            )
+        }
+
+        Spacer(
+            modifier = Modifier
+                .padding(top = 4.dp)
+                .fillMaxWidth()
+                .height(1.dp)
+                .background(Color.Black)
+        )
+    }
 }
 
-@Preview(showBackground = true)
+@UdemyFreeCoursesPreview
 @Composable
 fun GreetingPreview() {
     UdemyFreeCoursesTheme {
-        Greeting("Android")
+        SplashScreen()
     }
 }
