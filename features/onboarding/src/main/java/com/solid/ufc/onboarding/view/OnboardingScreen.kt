ff1a1ee11ae1da2fbb6c0fe364c21be8df421b5b
Nchekwube001 <unekwenchekwube@gmail.com>
2025-03-05 11:31:19 +0100

    wired in pager flow

--- features/onboarding/src/main/java/com/solid/ufc/onboarding/view/OnboardingScreen.kt
+++ features/onboarding/src/main/java/com/solid/ufc/onboarding/view/OnboardingScreen.kt
@@ -1,73 +1,192 @@
 package com.solid.ufc.onboarding.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.solid.ufc.onboarding.R
 import com.solid.ufc.onboarding.viewmodel.OnboardingViewModel
-import com.solid.ufc.ui.R
 import com.solid.ufc.ui.theme.PrimaryCardColors
 import com.solid.ufc.ui.theme.UFCTheme
 import com.solid.ufc.ui.utils.UFCPreview
+import com.solid.ufc.ui.utils.fourPadding
 import com.solid.ufc.ui.utils.onePadding
+import com.solid.ufc.ui.utils.threePadding
+import com.solid.ufc.ui.utils.twoPadding
+import kotlinx.coroutines.launch
 
+data class PagerItemData(
+    val title: String,
+    val description: String,
+    val image: Painter
+)
 @Composable
 fun OnboardingScreen(
     viewModel: OnboardingViewModel,
     rootNavController: NavController
 ) {
-    LaunchedEffect("goToHome") {
+
+    val items = listOf(
+        PagerItemData(
+            title= stringResource(R.string.pager_one_title),
+            description=stringResource(R.string.pager_one_desc),
+            image = painterResource(R.drawable.onb1)
+    ),     PagerItemData(
+            title= stringResource(R.string.pager_two_title),
+            description=stringResource(R.string.pager_two_desc),
+            image = painterResource(R.drawable.onb2)
+    ),     PagerItemData(
+            title= stringResource(R.string.pager_three_title),
+            description=stringResource(R.string.pager_three_desc),
+            image = painterResource(R.drawable.onb3)
+    ),
+    )
+    val pagerState = rememberPagerState(pageCount = {
+        items.size
+    })
+    val coroutineScope = rememberCoroutineScope()
+//    LaunchedEffect("goToHome") {
 //        viewModel.goToHome(rootNavController) //you can comment this out when you want to implement going to the home screen
-    }
+//    }
 
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(MaterialTheme.colorScheme.background)
-    ) {
-        Card(
-            modifier = Modifier
-                .onePadding()
-                .fillMaxSize(),
-            colors = PrimaryCardColors.copy(containerColor = MaterialTheme.colorScheme.background),
+     Scaffold (
+         topBar = {
+             Row(
+                 modifier = Modifier
+                     .fillMaxWidth()
+                     .fourPadding(),
+                 horizontalArrangement = Arrangement.SpaceBetween
+             ) {
+                 Spacer(Modifier.height(3.dp))
 
-            ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
+                 Text(text = stringResource(R.string.skip))
+             }
+         },
+bottomBar = {
 
+   Row(
+       horizontalArrangement = Arrangement.Center,
+       modifier = Modifier
+           .fillMaxWidth()
+           .padding(bottom = 70.dp)
+   ){
+       Box(
+           contentAlignment = Alignment.Center,
+           modifier = Modifier
+               .size(66.dp)
+               .background(
+                   color = Color(0xFF446EA6),
+                   shape = CircleShape
+               )
+               .clickable {
+                   if(pagerState.currentPage ==items.size-1){
 
-                Spacer(modifier = Modifier.weight(1f))
+                   }else{
+                      coroutineScope.launch {
+                          pagerState.animateScrollToPage(pagerState.currentPage+1)
+                      }
+                   }
+               }
+       ) {
+           if(pagerState.currentPage !=items.size-1 ){
+               Image(
+                   painter =painterResource(R.drawable.arrow_back),
+                   contentDescription = null,
+                   contentScale = ContentScale.Crop,
+                   modifier = Modifier.size(24.dp)
+               )
+           }
 
-                Text(
-                    stringResource(R.string.onboard),
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.scrim
-                )
-
-                Spacer(modifier = Modifier.weight(1f))
-            }
-        }
-    }
+           else{
+               Text(
+                   text = stringResource(R.string.start),
+                   color = Color.White
+               )
+           }
+       }
+   }
 }
 
-@Composable
-@UFCPreview
-fun OnboardingScreenPreview() {
-    UFCTheme {
-        OnboardingScreen(
-            viewModel = OnboardingViewModel(),
-            rootNavController = NavController(LocalContext.current)
-        )
-    }
+     ) { paddingValues ->
+
+         Column(
+             modifier = Modifier
+                 .fillMaxSize()
+                 .padding(paddingValues)
+                 .background(MaterialTheme.colorScheme.background)
+         ) {
+
+             HorizontalPager(state = pagerState) { page ->
+                 val item = items[page]
+                 Column(
+                     modifier = Modifier
+                         .fillMaxSize()
+                         .padding(16.dp),
+                     horizontalAlignment = Alignment.CenterHorizontally,
+                     verticalArrangement = Arrangement.Center
+                 ) {
+                     Image(
+                         painter = item.image,
+                         contentDescription = null,
+                         contentScale = ContentScale.Crop,
+                         modifier = Modifier.size(320.dp)
+                     )
+                     Spacer(modifier = Modifier.height(16.dp))
+                     Text(
+                         text = item.title,
+                         fontSize = 18.sp,
+                         textAlign = TextAlign.Center
+                     )
+                     Spacer(modifier = Modifier.height(12.dp))
+                     Text(
+                         text = item.description,
+                         fontSize = 14.sp,
+                         textAlign = TextAlign.Center
+                     )
+                 }
+             }
+         }
+     }
 }
+
+//@Composable
+//@UFCPreview
+//fun OnboardingScreenPreview() {
+//    UFCTheme {
+//        OnboardingScreen(
+//            viewModel = OnboardingViewModel(),
+//            rootNavController = NavController(LocalContext.current)
+//        )
+//    }
+//}
